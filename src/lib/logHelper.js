34fa8b572551d559d1853a788fb9b9ccdac2b895
Alex <alex.naga@hotmail.com>
2019-09-29 12:05:51 +0200

    Add pageLog

--- src/lib/logHelper.js
+++ src/lib/logHelper.js
@@ -12,8 +12,13 @@ const successMsg = (msg) => {
   console.log(chalk.green.bold('Success: ') + msg);
 };
 
+const pageLog = (msg) => {
+  console.log(chalk.yellow.bold('Page log: ') + msg);
+};
+
 module.exports = {
   errorMsg,
   infoMsg,
   successMsg,
+  pageLog,
 };
