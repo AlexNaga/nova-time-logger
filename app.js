49a7d3d6cb40db73343d43c3e053aff5bab8f137
Alex Naga <alex.naga@tiqqe.com>
2019-09-30 10:55:27 +0200

    Add messages as parameters -m <message>

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+const program = require('commander');
 const { errorMsg } = require('./src/lib/logHelper');
 const { TimeLogger } = require('./src/TimeLogger');
 
+const { env } = process;
+
 async function main() {
+  program
+    .option('-d, --debug', 'Output page logs and show browser')
+    .option('-m, --message <comment>', 'Activities done during the time period');
+
+  program.parse(process.argv);
+
+  if (program.debug === true) env.IS_DEBUG_MODE = true;
+  if (program.message) env.MESSAGE = program.message;
+
   try {
     const timeLogger = new TimeLogger();
     await timeLogger.run();
