3c92a3179d657e4ee618e75e219c292dbb5a7be8
Alex <alex.naga@postnord.com>
2020-09-15 17:21:15 +0200

    Add project and ioNumber as input params

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { errorMsg } from './lib/logHelper';
 (async () => {
   app
     .usage('[options] <command>')
-    .option('-m, --message <comment>', 'Activities done during the time period', '-')
+    .option('-m, --message <string>', 'Activities done during the time period', '-')
     .option('-d, --days <number>', 'Days to add or substract to todays date')
+    .option('-p, --project <string>', 'Project name')
+    .option('-io, --ioNumber <number>', 'The IO Number to report time to')
     .option('--debug', 'Show browser', false)
     .option('-l, --logs', 'Output page logs', false)
     .parse(process.argv);
