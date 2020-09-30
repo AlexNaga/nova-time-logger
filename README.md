afb7927187c362845368f09e5993500bbc1309a8
Alex <alex.naga@postnord.com>
2020-09-30 10:07:35 +0200

    Add default message

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 * Automatically login and log your time.
 * Support for a custom message.
+* Supports logging for the whole week.
+* Supports changing the number hours to report.
 * Possible to add or subtract to today's date.
 * Knows if you're trying to add a date to the weekend, which won't work.
 * Check if the date you're trying to add already exists.
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { errorMsg } from './lib/logHelper';
 (async () => {
   app
     .usage('[options] <command>')
-    .option('-m, --message <string>', 'Activities done during the time period', '-')
+    .option('-m, --message <string>', 'Activities done during the time period')
     .option('-d, --days <number>', 'Days to add or substract to todays date')
+    .option('-h, --hours <number>', 'Number of hours to add')
+    .option('-w, --week', 'If wanting to report for the whole week', false)
     .option('-p, --project <string>', 'Project name')
     .option('-io, --ioNumber <number>', 'The IO Number to report time to')
     .option('--debug', 'Show browser', false)
